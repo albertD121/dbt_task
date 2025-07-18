d5eb9f27c3e1cf21d83ee6174e3b29592d2b4b0b
Albert Domingo <domingo.albert26+dbt@gmail.com>
2025-07-18 14:55:34 +0000

    Breakdown of models

--- models/customer_orders.sql
+++ models/customer_orders.sql
@@ -6,50 +6,19 @@
 
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name,
-        age,
-        gender,
-        state,
-        postal_code,
-        country,
-        traffic_source
-
-    from `bigquery-public-data`.thelook_ecommerce.users
-    order by customer_id
+    select * from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
-    select
-        customers.country,
-        count(order_id) as number_of_orders,
-        status
-
-    from `bigquery-public-data`.thelook_ecommerce.orders
-    left join customers on customer_id = user_id 
-    group by customers.country, status
-    order by customers.country, number_of_orders DESC, status
+    select * from {{ ref('stg_orders') }}
 
 ),
 
 order_details as (
 
-    select
-    oi. product_id,
-    products.category,
-    products.name,
-    products.brand,
-    count(oi.order_id) as number_of_orders,
-    oi.status
-
-    from `bigquery-public-data`.thelook_ecommerce.order_items oi
-    left join `bigquery-public-data`.thelook_ecommerce.products on products.id = product_id
-    group by oi.product_id,products.category,products.name,products.brand,oi.status
-    order by number_of_orders DESC, status DESC
+    select * from {{ ref('stg_orderDetails') }}
 
 ),
 
